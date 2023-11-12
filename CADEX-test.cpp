2414d47d2ea7ee6afc166edc99a0765fa96c1e78
Ted <tedofwar@gmail.com>
2023-11-12 17:36:20 +0300

    Initial commit

---
+++ CADEX-test.cpp
@@ -0,0 +1,12 @@
+﻿// CADEX-test.cpp : Defines the entry point for the application.
+//
+
+#include "CADEX-test.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
